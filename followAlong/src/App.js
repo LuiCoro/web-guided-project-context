81cd3abdd8880905096efa8ee0ddba12b02ea34d
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-09-09 13:35:22 -0400

    lecture work-in-progress

--- followAlong/src/App.js
+++ followAlong/src/App.js
@@ -14,7 +14,9 @@ export default function App() {
 
   return(
     <div className="App">
-      <h1>Family Trees</h1>
+      <section className="header">
+        <h1>Family Trees</h1>
+      </section>
   </div>);
 }
 
